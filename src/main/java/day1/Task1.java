5321d8d1d74a566dcd14e501133ab0c93309d04c
Sergey Modin <sergeymodin@Andrews-Mac.local>
2020-11-29 21:41:43 -0800

    Add Task1-6 solutions

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int x = 1;
+        while (x < 11) {
+            System.out.print("JAVA ");
+            x++;
+        }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 4;
+        int j;
+        for (int i = 1; i < 10; i++) {
+            j = i * k;
+            System.out.println(i + " x " + k + " = " + j);
+        }
     }
 }
